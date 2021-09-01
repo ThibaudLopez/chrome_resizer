e3175c6aa10075d97be204f0068b8f5f3c4feef3
Thibaud Lopez Schneider <thibaud.lopez.schneider@gmail.com>
2021-08-31 23:03:03 -0700

    added keyboard shortcuts ALT+R and ALT+SHIFT+R

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+// list of common resolutions
+// https://en.wikipedia.org/wiki/List_of_common_resolutions#/media/File:Vector_Video_Standards.svg
+var resolutions = [
+	// 4:3 ratios
+	// { width: 640,  height: 480  },
+	// { width: 800,  height: 600  },
+	{ width: 1024, height: 768  },
+	{ width: 1280, height: 960  },
+	{ width: 1600, height: 1200 },
+	{ width: 2048, height: 1536 },
+];
+
+// default resolution
+var default_resolution = 1;
+
 // snap to margin
 var margin = 0.10; // 10%
 
@@ -8,27 +23,49 @@ function within(value, margin /*[0,1]*/, target) {
 	return (value >= low && value <= high);
 }
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var sizes = [
-		// 4:3 ratio
-		// { width: 640,  height: 480  },
-		// { width: 800,  height: 600  },
-		// { width: 1024, height: 768  },
-		{ width: 1280, height: 960  },
-		{ width: 1600, height: 1200 },
-		{ width: 2048, height: 1536 },
-	];
-	// current size ?
-	var i = sizes.findIndex(s => within(tab.width, margin, s.width) && within(tab.height, margin, s.height));
-	// next size ?
-	var next;
-	if (i === -1 || i === sizes.length - 1) {
-		// default size
-		next = sizes[0];
-	} else {
-		// next size up
-		next = sizes[i + 1];
-	}
-	// resize
-	chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, { width: next.width, height: next.height });
+// find this Chrome window's closest resolution from the array of resolutions, and returns the array index
+function get_closest_resolution(window) {
+	return resolutions.findIndex(s => within(window.width, margin, s.width) && within(window.height, margin, s.height));
+}
+
+// resize this Chrome window in the specified direction, up or down
+function resize(command) {
+	chrome.windows.getCurrent(window => {
+		// current resolution?
+		var i = get_closest_resolution(window);
+		// next resolution?
+		var resolution;
+		if (i === -1) {
+			// default
+			resolution = resolutions[default_resolution];
+		} else {
+			if (command === "resize_down") {
+				if (i === 0) {
+					// rollover
+					resolution = resolutions[resolutions.length - 1];
+				} else {
+					// smaller
+					resolution = resolutions[i - 1];
+				}
+			} else if (command === "resize_up") {
+				if (i === resolutions.length - 1) {
+					// rollover
+					resolution = resolutions[0];
+				} else {
+					// bigger
+					resolution = resolutions[i + 1];
+				}
+			}
+		}
+		// resize
+		chrome.windows.update(chrome.windows.WINDOW_ID_CURRENT, { width: resolution.width, height: resolution.height });
+	});
+}
+
+chrome.browserAction.onClicked.addListener((tab) => {
+	resize("resize_up");
+});
+
+chrome.commands.onCommand.addListener((command) => {
+	resize(command);
 });
